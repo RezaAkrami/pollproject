d7e6828fbe13fbdbe00e15b280cc2aa6bfc615f2
Reza Akrami <RezaAkrami32@gmail.com>
2022-08-19 16:46:34 +0430

    added styles to project

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,34 +1,55 @@
 import React from 'react';
+import styles from '../style/poll.module.scss';
 
 const Poll = () => {
     return (
-        <form>
-            <div className="container">
-                <h1>Dear user</h1>
-                <p>Please participate in the survey to improve the quality of the services provided to you</p>
-
-                <div className="poll">
-                    <h4>Click on the image you want</h4>
-
-                    <div className="pictures">
-
+        <div>
+            <form action="">
+                <div className={styles.container}>
+                    <div className={styles.texts}>
+                        <h1>Dear user</h1>
+                        <p>Please participate in the survey to improve the quality of the services provided to you</p>
                     </div>
-                    <div className="Points">
-                        <span id="positive">Positive</span>
-                        <span id="negative">Negative</span>
-                        <div className="questions">
+
+                    <div className={styles.poll}>
+
+                        <h4>Click on the image you want</h4>
+
+                        <div className={styles.icons}>
+                            <i className="far fa-grin-hearts"></i>
+                            <i className="far fa-smile-beam"></i>
+                            <i className="far fa-meh"></i>
+                            <i className="far fa-frown"></i>
+                            <i className="far fa-angry"></i>
+                        </div>
+
+                        <div className={styles.points}>
+                            <div className={styles.btn}>
+                                <span id={styles.positive}>Positive</span>
+                                <span id={styles.negative}>Negative</span>
+                            </div>
+
+                            <div className={styles.questions}>
+                                <span>reza</span>
+                                <span>reza</span>
+                                <span>reza</span>
+                                <span>reza</span>
+                                <span>reza</span>
+                                <span>reza</span>
+                                <span>reza</span>
+                                <span>reza</span>
+                            </div>
 
                         </div>
 
+                        <textarea name="text" rows="10" placeholder='Enter your text ...'></textarea>
+                        <br />
+                        <button type='submit'>send</button>
+
                     </div>
-
-                    <textarea name="text" id="text" rows="10" placeholder='Enter your text ...'></textarea>
-                    <br />
-                    <button>send</button>
-
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 };
 
