9755be7c0ede0e6ea98db30b77a45476c9ef96c1
Reza Akrami <RezaAkrami32@gmail.com>
2022-08-19 18:25:59 +0430

    Entry of the quality level into the state

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React , {useState} from 'react';
 import styles from '../style/poll.module.scss';
 
 const Poll = () => {
+
+    const [ quality , setQuality ] = useState("");
+
+    // Entry of the quality level into the state - Five out of five
+    const qualityHandler = e =>{
+        let target = e.target;
+
+        if(target.classList.contains("fa-grin-hearts")){
+            setQuality("5");
+        }else if(target.classList.contains("fa-smile-beam")){
+            setQuality("4");
+        }else if(target.classList.contains("fa-meh")){
+            setQuality("3");
+        }else if(target.classList.contains("fa-frown")){
+            setQuality("2");
+        }else if(target.classList.contains("fa-angry")){
+            setQuality("1");
+        }
+    };  
+
     return (
         <div>
             <form action="">
@@ -15,12 +35,12 @@ const Poll = () => {
 
                         <h4>Click on the image you want</h4>
 
-                        <div className={styles.icons}>
-                            <i className="far fa-grin-hearts"></i>
-                            <i className="far fa-smile-beam"></i>
-                            <i className="far fa-meh"></i>
-                            <i className="far fa-frown"></i>
-                            <i className="far fa-angry"></i>
+                        <div className={styles.icons} onClick={qualityHandler}>
+                            <i id={quality==="5" ? styles.green : null } className="far fa-grin-hearts"></i>
+                            <i id={quality==="4" ? styles.greenyellow : null } className="far fa-smile-beam"></i>
+                            <i id={quality==="3" ? styles.black : null } className="far fa-meh"></i>
+                            <i id={quality==="2" ? styles.orange : null } className="far fa-frown"></i>
+                            <i id={quality==="1" ? styles.red : null } className="far fa-angry"></i>
                         </div>
 
                         <div className={styles.points}>
